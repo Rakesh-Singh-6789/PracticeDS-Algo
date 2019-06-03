b587a141c54133c82aa8de63c29ef9ccd46348e9
Rakesh Singh <rakeshsr6789@gmail.com>
2019-06-04 02:29:42 +0530

    Updated CheckFirst

--- src/Assignment1/CheckFirst.java
+++ src/Assignment1/CheckFirst.java
@@ -5,24 +5,7 @@ public class CheckFirst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-   int odd,even,n;
-        for(int i=1;i<=n;i++){
-            
-            if(odd<=n){
-                int l=n*odd;                
-                for(int j=l-n;j<l;j++){
-                    out.print(j+" ");
-                }
-                odd+=2;
-            }else{
-                 int l=n*odd;                
-                for(int j=l-n;j<l;j++){
-                    out.print(j+" ");
-                }
-                odd+=2;
-            }
-            
-        }    
+		boolean chArray[] = new boolean[15];
+		System.out.println(chArray[14]+" : "+chArray[0]);
 	}
 }
