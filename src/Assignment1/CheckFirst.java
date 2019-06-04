b91f5a6e2334481ae89e20a77296cbb07d1e0f61
Rakesh Singh <rakeshsr6789@gmail.com>
2019-06-05 00:25:28 +0530

    Updated CheckFirst

--- src/Assignment1/CheckFirst.java
+++ src/Assignment1/CheckFirst.java
@@ -4,12 +4,25 @@ public class CheckFirst {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		int[] arr = new int[5];
 		arr = new int[6];
-		
-		String str="abcdefgh";
-		System.out.println(str.substring(3, 7));
-	}
+		int size=arr.length;
+
+		String str="aba";
+		/*System.out.println(str.substring(3, 8));
+
+		String myStr[]=str.split("");
+
+		System.out.println(myStr.length);*/
 	
+		StringBuilder sb=new StringBuilder(str);
+        sb=sb.reverse();
+        
+        System.out.println(str.equals(sb.toString()));
+
+
+
+	}
+
 }
