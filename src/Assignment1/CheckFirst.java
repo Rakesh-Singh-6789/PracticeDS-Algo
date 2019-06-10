903b0c0054ece6829d53541bc31ea933ea3c6920
Rakesh Singh <rakeshsr6789@gmail.com>
2019-06-10 16:30:13 +0530

    Updated CheckFirst

--- src/Assignment1/CheckFirst.java
+++ src/Assignment1/CheckFirst.java
@@ -1,5 +1,6 @@
 package Assignment1;
 
+import java.math.BigInteger;
 import java.util.*;
 
 public class CheckFirst {
@@ -29,7 +30,7 @@ public class CheckFirst {
 		//long max=Long.MIN_VALUE;
 		//System.out.println(max);
 		
-		HashMap<Integer,Integer> map=new HashMap<>();
+		/*HashMap<Integer,Integer> map=new HashMap<>();
 		
 		int d=Math.abs(5-6);
 		
@@ -44,7 +45,64 @@ public class CheckFirst {
 		}
 		int m=map.getOrDefault(5, 10);
 		System.out.println(m);
-		System.out.println((int)Math.ceil(0.75*9));
+		System.out.println((int)Math.ceil(0.75*9));*/
+		
+		
+		/*int n=1000000000;
+		int array[]=new int[n];*/
+		
+		double val;
+		int modulus=1000000007;
+		
+		/*for(int i=1;i<=50;i++){
+			double pow=1.0*i*1000;
+			val=(Math.pow(2.0,pow)%modulus);
+			System.out.println(i+" "+val+"  "+pow);
+		}*/
+		
+		/*System.out.println((long)(Math.pow(2, 1000)%modulus));
+		long ans=1;
+		for(int i=1;i<=2000;i++){
+			ans=(ans*2)%modulus;
+		}
+		System.out.println(ans);
+		
+		BigInteger mod=new BigInteger("1000000007");
+	    BigInteger bi=new BigInteger("688423210");
+		System.out.println(new BigInteger("688423210").multiply(bi).mod(mod));*/
+		
+		System.out.println((int)(Math.pow(2, 100)%modulus));
+		
+		/*int count=0;
+		for(int i=0;i<1000;i++){
+			HashMap<Integer,Integer> map=new HashMap<>();
+			int num=i;
+			int ud=num%10;
+			map.put(ud, 1);
+			int td=(num%100)/10;
+			map.put(td, 1);
+			int hd=(num/100);
+			map.put(hd, 1);
+			
+			num=999-i;
+			ud=num%10;
+			map.put(ud, 1);
+			td=(num%100)/10;
+			map.put(td, 1);
+			hd=(num/100);
+			map.put(hd, 1);
+			
+			if(map.size()==2){
+				count++;
+			}
+		}
+		
+		System.out.println(count);*/
+		
+		
+		
+		
+		
 		/*if(map.containsKey(1)){
 			map.put(1,map.get(1));
 		}
