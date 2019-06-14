e75fc3ada55b875d9ed4de3ab989933fd4715501
Rakesh Singh <rakeshsr6789@gmail.com>
2019-06-14 23:17:49 +0530

    updated count pall substring

--- src/Assignment2/Q91_CountPallSubString.java
+++ src/Assignment2/Q91_CountPallSubString.java
@@ -5,29 +5,74 @@ public class Q91_CountPallSubString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(countPalindromeSubstrings("ababab"));
+		System.out.println(countPalindromeSubstrings("aaaa"));
 
 	}
 	
-	public static int countPalindromeSubstrings(String s) {
-		// Write your code here
-        int length=s.length();
-        int count=length;
-        System.out.println(count);
-        for(int i=0;i<length;i++){
-            
-            
-            for(int j=i+2;j<=length;j++){
-                System.out.println(s.substring(i,j));
-                StringBuilder sb=new StringBuilder(s.substring(i,j));
-                
-                if(sb.reverse().toString().equals(s.substring(i,j))){
-                	System.out.println("true");
-                    count++;
-                }
-            }
-        }
-       return count;
-	}
+	 public static int countPalindromeSubstrings(String s) {
+	        // Write your code here
 
+	        int length=s.length();
+	        int count=length;
+
+	        //for odd substrings
+	        int mid=1;        
+
+	        while(mid<length){
+	            int left=mid-1;
+	            int right=mid+1;
+	            while(left>-1 && right<length){
+	                if(s.charAt(left)==(s.charAt(right))){
+	                    count++;
+	                    left--;
+	                    right++;
+	                }else{
+	                    break;
+	                }       
+
+	            }
+	            mid++;
+	        }
+
+//System.out.println("odd:"+count);
+	        //for even 
+	        int midL=0;
+	        int midR=1;	       
+
+	            while(midR<length){
+	            	
+	            	 if(s.charAt(midL)==s.charAt(midR)){
+		    	        
+		    	            count++;
+		    	            
+		    	            int leftO=midL-1;
+			                int rightO=midR+1;
+			               
+
+			                while(leftO>-1 && rightO<length){       
+			                	
+			                    if(s.charAt(leftO)==s.charAt(rightO)){
+			                        count++;
+			                        leftO--;
+			                        rightO++;
+			                    }else{
+			                        break;
+			                    }        
+
+			                }
+		    	            
+		                }
+	            	
+	                midL++;
+	                midR++;
+	            }
+	        
+
+
+
+
+
+
+	        return count;
+	    }
 }
