4091f7f96c3103c1f896a7f794933956e629c2ad
Rakesh Singh <rakeshsr6789@gmail.com>
2019-06-23 22:33:41 +0530

    updated CheckFirst .java

--- src/Assignment1/CheckFirst.java
+++ src/Assignment1/CheckFirst.java
@@ -5,21 +5,14 @@ import java.util.*;
 
 public class CheckFirst {
 
-	public static void print(int n){
-	    if(n < 0){
-	        return;
-	    }
-	    if(n == 0){
-	        System.out.println(n);
-	        return;
-	    }
-	    print(n-1);
-	    System.out.print(n+" ");
-	}
+	
 
 	public static void main(String[] args) {
-	    int num = 3;
-	    print(num);
+	   ArrayList<Integer> list=new ArrayList<>();
+	   
+	   Object[] arr=list.toArray();
+	   int ouput[];
+	   
 	}
 
 }
