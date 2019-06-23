075ae661d92f84e48ecbcfee3024d1cf91d65b08
Rakesh Singh <rakeshsr6789@gmail.com>
2019-06-23 22:34:40 +0530

    Added Assignment 4 Q1 and Q2

---
+++ src/Assignment4_recursion1/Q1_returnLastIndex.java
@@ -0,0 +1,46 @@
+package Assignment4_recursion1;
+
+public class Q1_returnLastIndex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]={1,2,5,6,5,9};
+		
+		System.out.println(firstIndex(arr,5));
+
+	}
+	
+	 public static int firstIndex(int input[], int x) {
+	        /* Your class should be named Solution
+			 * Don't write main().
+			 * Don't read input, it is passed as function argument.
+			 * Return output and don't print it.
+		 	 * Taking input and printing output is handled automatically.
+			*/
+
+	        return firstIndex(input,x,0);
+
+	    }
+
+	    public static int firstIndex(int input[], int x,int startIndex) {
+
+	      if(startIndex>=input.length){
+	          return -1;
+	      } 
+	        
+	      
+
+	        
+	       int index=firstIndex(input,x,startIndex+1);
+	        
+	       if(index==-1 && input[startIndex]==x){
+	          return startIndex;
+	       }
+	       
+	       return index;
+	        
+	        
+	      
+	    }
+
+	}
---
+++ src/Assignment4_recursion1/Q2_returnAllIndexes.java
@@ -0,0 +1,47 @@
+package Assignment4_recursion1;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Q2_returnAllIndexes {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]={1,2,5,6,5,9};
+		
+		allIndexes(arr,5);
+
+	}
+
+	public static void allIndexes(int input[], int x) {
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+	 	 * Taking input and printing output is handled automatically.
+		*/
+       
+         
+        ArrayList<Integer> list=new ArrayList<>();        
+		ArrayList<Integer> ans= allIndexes(input,x,list,0);
+		int ans[]=new int[7];
+		System.out.println(ans);
+	}
+    public static ArrayList<Integer> allIndexes(int input[], int x,ArrayList<Integer> output,int startIndex) {
+		
+        
+        if(startIndex>=input.length){
+            return output;
+        }
+        
+        if(input[startIndex]==x){
+          output.add(startIndex);
+        }
+        
+        return allIndexes(input,x,output,startIndex+1);
+        
+        
+	}
+    
+	
+}
